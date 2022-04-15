569de6de05d383476b4b2b01d85383f9716024eb
ZZDirty <muztaga2@gmail.com>
2022-04-15 21:28:42 +0800

    add lab3 codes

--- intern.cpp
+++ intern.cpp
@@ -2,46 +2,144 @@
 #include "ast.h"
 #include "semantic.h"
 #include <iostream>
+#include <sstream>
 
-void IRGenerator::addStructures(StructDefTable& structs) {
-  
+std::ostream &operator<<(std::ostream &os, const Operand &operand) {
+
+    return os;
 }
 
-void IRGenerator::addFunctions(FunctionTable& funcs) {
-
+void IRGenerator::addStructures(StructDefTable &structs) {
+    this->structs = structs;
 }
 
+void IRGenerator::addFunctions(FunctionTable &funcs) {
+    this->funcs = funcs;
+}
 
 void IRGenerator::genIR(pASTNode node) {
-  if (node == nullptr) {
-    return;
-  }
-  if (!strcmp(node->name, "ExtDef")) {
-    genExtDef(node);
-  }
+    if (node == nullptr) {
+        return;
+    }
+    if (!strcmp(node->name, "ExtDef")) {
+        genExtDef(node);
+    }
 
-  for (size_t i = 0; i < node->child_count; i++) {
-    genIR(node->child[i]);
-  }
+    for (size_t i = 0; i < node->child_count; i++) {
+        genIR(node->child[i]);
+    }
 }
 
 void IRGenerator::genExtDef(pASTNode extdef) {
-  // 没有全局变量,只处理FunDec CompSt;
-  /*
-        Specifier ExtDecList SEMI
-      | Specifier SEMI
-      | Specifier FunDec CompSt
-      | Specifier FunDec SEMI
-  */
-  if (extdef->child_count == 3 && !strcmp(extdef->child[2]->name, "CompSt")) {
-    genFunDec(extdef->child[1]);
-    genCompSt(extdef->child[2]);
-  }
+    // 没有全局变量,只处理FunDec CompSt;
+    /*
+          Specifier ExtDecList SEMI
+        | Specifier SEMI
+        | Specifier FunDec CompSt
+        | Specifier FunDec SEMI
+    */
+    if (extdef->child_count == 3 && !strcmp(extdef->child[2]->name, "CompSt")) {
+        genFunDec(extdef->child[1]);
+        genCompSt(extdef->child[2]);
+    }
 }
 
-void IRGenerator::genFunDec(pASTNode fundec) {
+void IRGenerator::genFunDec(pASTNode funDec) {
 
 }
-void IRGenerator::genCompSt(pASTNode compst) {
+
+void IRGenerator::genCompSt(pASTNode compSt) {
 
 }
+
+Operand InterCodeList::newTemp() {
+    Operand op;
+    op.kind = OpKind::OP_VARIABLE;
+    std::stringstream ss;
+    ss << "t" << labelNumber;
+    op.name = ss.str();
+    labelNumber += 1;
+    return op;
+}
+
+void InterCodeList::genIRCode(IRKind kind, std::initializer_list<Operand> ops) {
+    Operand temp;
+    Operand result, op1, op2, relop;
+    int size = 0;
+    InterCode newCode;
+
+    switch (kind) {
+        case IR_LABEL:
+        case IR_FUNCTION:
+        case IR_GOTO:
+        case IR_RETURN:
+        case IR_ARG:
+        case IR_PARAM:
+        case IR_READ:
+        case IR_WRITE:
+            assert(ops.size() == 1);
+            op1 = *ops.begin();
+            if (op1.kind == OP_ADDRESS) {
+                temp = newTemp();
+                genIRCode(IR_READ_ADDR, {temp, op1});
+                op1 = temp;
+            }
+            newCode.genInterCode(kind, {op1});
+            codes.push_back(newCode);
+            break;
+        case IR_ASSIGN:
+        case IR_GET_ADDR:
+        case IR_READ_ADDR:
+        case IR_WRITE_ADDR:
+        case IR_CALL:
+            op1 = *ops.begin();
+            op2 = *(ops.begin() + 1);
+            if (kind == IR_ASSIGN &&
+                (op1.kind == OP_ADDRESS || op2.kind == OP_ADDRESS)) {
+                if (op1.kind == OP_ADDRESS && op2.kind != OP_ADDRESS)
+                    genIRCode(IR_WRITE_ADDR, {op1, op2});
+                else if (op2.kind == OP_ADDRESS && op1.kind != OP_ADDRESS)
+                    genIRCode(IR_READ_ADDR, {op1, op2});
+                else {
+                    temp = newTemp();
+                    genIRCode(IR_READ_ADDR, {temp, op2});
+                    genIRCode(IR_WRITE_ADDR, {op1, temp});
+                }
+            } else {
+                newCode.genInterCode(kind, {op1, op2});
+                codes.push_back(newCode);
+            }
+            break;
+        case IR_ADD:
+        case IR_SUB:
+        case IR_MUL:
+        case IR_DIV:
+            result = *ops.begin();
+            op1 = *(ops.begin() + 1);
+            op2 = *(ops.begin() + 2);
+            if (op1.kind == OP_ADDRESS) {
+                temp = newTemp();
+                genIRCode(IR_READ_ADDR, {temp, op1});
+                op1 = temp;
+            }
+            if (op2.kind == OP_ADDRESS) {
+                temp = newTemp();
+                genIRCode(IR_READ_ADDR, {temp, op2});
+                op2 = temp;
+            }
+            newCode.genInterCode(kind, {result, op1, op2});
+            codes.push_back(newCode);
+            break;
+
+        case IR_DEC:
+            newCode.genInterCode(kind, ops);
+            codes.push_back(newCode);
+            break;
+
+        case IR_IF_GOTO:
+            newCode.genInterCode(kind, ops);
+            codes.push_back(newCode);
+            break;
+    }
+}
+
--- intern.hpp
+++ intern.hpp
@@ -10,75 +10,157 @@
 #include <vector>
 
 enum OpKind {
-  OP_VARIABLE,
-  OP_CONSTANT,
-  OP_ADDRESS,
-  OP_LABEL,
-  OP_FUNCTION,
-  OP_RELOP,
+    OP_VARIABLE,
+    OP_CONSTANT,
+    OP_ADDRESS,
+    OP_LABEL,
+    OP_FUNCTION,
+    OP_RELOP,
+    OP_SIZE,
 };
 
 struct Operand {
-  OpKind kind;
-  int value;
-  std::string name;
+    OpKind kind;
+    int value;
+    std::string name;
+
+    friend std::ostream &operator<<(std::ostream &os, const Operand &operand);
 };
 
 enum IRKind {
-  IR_LABEL,
-  IR_FUNCTION,
-  IR_ASSIGN,
-  IR_ADD,
-  IR_SUB,
-  IR_MUL,
-  IR_DIV,
-  IR_GET_ADDR,
-  IR_READ_ADDR,
-  IR_WRITE_ADDR,
-  IR_GOTO,
-  IR_IF_GOTO,
-  IR_RETURN,
-  IR_DEC,
-  IR_ARG,
-  IR_CALL,
-  IR_PARAM,
-  IR_READ,
-  IR_WRITE,
+    IR_LABEL,
+    IR_FUNCTION,
+    IR_ASSIGN,
+    IR_ADD,
+    IR_SUB,
+    IR_MUL,
+    IR_DIV,
+    IR_GET_ADDR,
+    IR_READ_ADDR,
+    IR_WRITE_ADDR,
+    IR_GOTO,
+    IR_IF_GOTO,
+    IR_RETURN,
+    IR_DEC,
+    IR_ARG,
+    IR_CALL,
+    IR_PARAM,
+    IR_READ,
+    IR_WRITE,
 };
 
 struct InterCode {
-  IRKind kind;
-  struct {
-    Operand op;
-  } oneOp;
-  struct {
-    Operand right, left;
-  } assign;
-  struct {
-    Operand result, op1, op2;
-  } binOp;
-  struct {
-    Operand x, relop, y, z;
-  } ifGoto;
-  struct {
-    Operand op;
-    int size;
-  } dec;
+    IRKind kind;
+    struct {
+        Operand op;
+    } oneOp;
+    struct {
+        Operand right, left;
+    } assign;
+    struct {
+        Operand result, op1, op2;
+    } binOp;
+    struct {
+        Operand x, relop, y, z;
+    } ifGoto;
+    struct {
+        Operand op;
+        int size;
+    } dec;
+
+    friend std::ostream &operator<<(std::ostream &os, const InterCode &code) {
+        os << "kind: " << code.kind;
+        return os;
+    }
+
+    /**
+     * 对于 IR_DEC 需要传入的是op1 value
+     * @param kind
+     * @param ops
+     */
+    void genInterCode(IRKind kind, std::initializer_list<Operand> ops) {
+        this->kind = kind;
+        switch (kind) {
+            case IR_LABEL:
+            case IR_FUNCTION:
+            case IR_GOTO:
+            case IR_RETURN:
+            case IR_ARG:
+            case IR_PARAM:
+            case IR_READ:
+            case IR_WRITE:
+                assert(ops.size() == 1);
+                this->oneOp.op = *ops.begin();
+                break;
+            case IR_ASSIGN:
+            case IR_GET_ADDR:
+            case IR_READ_ADDR:
+            case IR_WRITE_ADDR:
+            case IR_CALL:
+                assert(ops.size() == 2);
+                this->assign.left = *ops.begin();
+                this->assign.right = *(ops.begin() + 1);
+                break;
+            case IR_ADD:
+            case IR_SUB:
+            case IR_MUL:
+            case IR_DIV:
+                assert(ops.size() == 3);
+                this->binOp.result = *ops.begin();
+                this->binOp.op1 = *(ops.begin() + 1);
+                this->binOp.op2 = *(ops.begin() + 2);
+                break;
+            case IR_DEC:
+                assert(ops.size() == 2);
+                this->dec.op = *(ops.begin());
+                this->dec.size = (ops.begin() + 1)->value;
+                break;
+            case IR_IF_GOTO:
+                assert(ops.size() == 4);
+                this->ifGoto.x = *ops.begin();
+                this->ifGoto.relop = *(ops.begin() + 1);
+                this->ifGoto.y = *(ops.begin() + 2);
+                this->ifGoto.z = *(ops.begin() + 3);
+        }
+    }
 };
 
 struct InterCodeList {
-  int tempVarNumber;
-  int labelNumber;
-  std::vector<InterCode> codes;
+    int tempVarNumber;
+    int labelNumber;
+    std::vector<InterCode> codes;
+
+    InterCodeList() {
+        tempVarNumber = 1;
+        labelNumber = 1;
+    }
+
+    Operand newTemp();
+
+    void genIRCode(IRKind kind, std::initializer_list<Operand> ops);
+
+    void printIRCodes() {
+        for (const auto &item: codes) {
+            std::cout << item << std::endl;
+        }
+    }
 };
 
 class IRGenerator {
 private:
+    InterCodeList codes;
+    StructDefTable structs;
+    FunctionTable funcs;
 public:
-  void addStructures(StructDefTable &structs);
-  void addFunctions(FunctionTable &funcs);
-  void genIR(pASTNode root);
-  void genExtDef(pASTNode root);
-  void genFunDec(pASTNode fundec);
-  void genCompSt(pASTNode compst);
+    void addStructures(StructDefTable &structs);
+
+    void addFunctions(FunctionTable &funcs);
+
+    void genIR(pASTNode root);
+
+    void genExtDef(pASTNode root);
+
+    void genFunDec(pASTNode funDec);
+
+    void genCompSt(pASTNode compSt);
 };
